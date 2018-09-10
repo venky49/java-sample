a7f5759565d984c343dc8574709dd1f4a46cbc64
Pasupureddy Koti <koti.pasupureddy@thomsonreuters.com>
2018-09-10 08:29:31 +0530

    new changes by new developer

--- src/main/java/com/zuora/zilla/App.java
+++ src/main/java/com/zuora/zilla/App.java
@@ -6,12 +6,12 @@ import java.io.InputStream;
 public class App {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("Hay Guys, I just made some changes to the code, now this should be new build output");
+		System.out.println("new code changes done by new developer, hello world has been replaced by hello koti");
 		InputStream is = App.class.getClassLoader().getResourceAsStream("toto.txt");
 		byte[] b = new byte[is.available()];
 		is.read(b);
 		String text = new String(b);
 		System.out.println(text);
 	}
-	
+
 }
